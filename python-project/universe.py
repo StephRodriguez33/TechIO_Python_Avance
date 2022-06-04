c8f23424585b4cd669a6f3fe3e6d62214d5a4d7f
StephRodriguez33 <106867289+StephRodriguez33@users.noreply.github.com>
2022-06-04 13:49:55 +0200

    Update universe.py

--- python-project/universe.py
+++ python-project/universe.py
@@ -1,5 +1,5 @@
 def count_all_stars(galaxies):
     total_stars = 0
     for stars in galaxies:
-        total_stars = stars  # fix me!
+        total_stars = stars  # tu devrais modifier cette ligne de code!
     return total_stars
